d59054bc30af0e93cc97e1d62cf0badea58531eb
lonetwin <lonetwin@4dd687d4-f136-0410-bce9-01c9dc5a8ffe>
2007-08-06 20:28:36 +0000

    - Cleaner special handling of last strip/frame. - Removed requirement for the image file to be in the current directory - Added check for PIL module - Wrapped up print statements in debug and set debug=0 by default

--- snipstrip.py
+++ snipstrip.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 """
 snipstrip.py: Command-line tool to split up images of comic strips into
-individual frames. This is useful to read comics on handheld devices such as
-mobile phones, PDAs and handheld multimedia players.
+individual frames. This is useful for reading comics on handheld devices such
+as mobile phones, PDAs and handheld multimedia players.
 
 Copyright 2007 by Steven Fernandez <steve@lonetwin.net>
 
@@ -20,17 +20,31 @@ You should have received a copy of the GNU General Public License along with
 this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import sys
-import Image
+import os
+try:
+    import Image
+except:
+    print """
+Error: This script uses the Python Imaging Library. Please install it.
+
+On rpm based linux distributions, the package is most probably called
+'python-imaging'. You may obtain the latest version of the library for your
+platform from the Python Imaging Library website:
+
+http://www.pythonware.com/products/pil/
+
+"""
+    sys.exit(1)
 
 usage = """
 %s <filename> [...]
 
-where arguments are the image files in the *current* directory (as of this
-release, sorry).
+where the arguments are comic strip image filenames.
 
-This script will create files named 00-<filename> to NN-<filename> in the
-current directory (where NN is the number of frames in the comic strip.
+This script will create files named 00-<filename> to NN-<filename> in the same
+directory as the image file.
 """ % sys.argv[0]
+
 
 def gen_map_on_xaxis(img, x_cord):
     color_map = {}
@@ -52,7 +66,9 @@ def gen_map_on_yaxis(img, y_cord):
 
 def snip(filename):
     y_candidates = []
+    odir, ofile  = os.path.split(os.path.abspath(filename))
 
+    debug  = 0  # Print debug messages
     img    = Image.open(filename)
     bw_img = img.convert('1') # Convert to black+white
     width, height = img.size[0], img.size[1]
@@ -73,37 +89,35 @@ def snip(filename):
         if (len(color_map.keys()) < 2):
             y_candidates.append(y_cord)
 
+    # For the frames of the bottom-most strip, always snip at co-ordinate
+    # 'height'.
+    y_candidates.append(height)
+
     # The second stage of the script. We start splitting the image along the
     # y-axis.
     strips = []
     crop_at = start_at = 0
     # start by examining the candidate co-ordinates ...
-    print "y candidates: ", y_candidates
-    for i, y_cord in enumerate(y_candidates[:-1]):
+    if debug: print "y candidates: ", y_candidates
+    for i, y_cord in enumerate(y_candidates):
         if y_cord == 0:
             continue
         crop_at = y_cord
+        # If the current candidate co-ordinate is too close to the start
+        # co-ordinate (ie: difference is less than 10px) skip it.
+        if (crop_at - start_at) < 10:
+            continue
         # If the current candidate co-ordinate is too close to the next one
         # (ie: difference is less than 10px) skip it.
-        # print y_candidates[i+1], y_cord
-        if ((y_candidates[i+1] - y_cord) < 10):
-            crop_at = y_candidates[i+1]
-            continue
-        elif ((crop_at - start_at) < 10):
+        if (i+1 < len(y_candidates)) and ((y_candidates[i+1] - y_cord) < 10):
             crop_at = y_candidates[i+1]
             continue
         else:
-            print "crop()ing at ", (0, start_at, width, crop_at)
+            if debug: print "crop()ing at ", (0, start_at, width, crop_at)
             c = img.crop((0, start_at, width, crop_at))
             #c.show()
             strips.append(c)
-            start_at = y_cord
-
-    # Last frame.
-    print "crop()ing at ", (0, start_at, width, height)
-    c = img.crop((0, start_at, width, height))
-    #c.show()
-    strips.append(c)
+            start_at = crop_at
 
     # Now take each individually split 'strip' and split it along the x-axis
 
@@ -119,33 +133,30 @@ def snip(filename):
             if (len(color_map.keys()) < 2):
                 x_candidates.append(x_cord)
 
-        print "x candidates: ", x_candidates
+        # For the left-most frames of each strip always snip at co-ordinate
+        # 'width'.
+        x_candidates.append(width)
+
+        if debug: print "x candidates: ", x_candidates
         crop_at = start_at = 0
-        for i, x_cord in enumerate(x_candidates[:-1]):
+        for i, x_cord in enumerate(x_candidates):
             if x_cord == 0:
                 continue
             crop_at = x_cord
-            print x_candidates[i+1], x_cord
-            if ((x_candidates[i+1] - x_cord) < 10):
-                crop_at = x_candidates[i+1]
+            if (crop_at - start_at) < 10:
                 continue
-            elif ((crop_at - start_at) < 10):
+            if (i+1 < len(x_candidates)) and ((x_candidates[i+1] - x_cord) < 10):
                 crop_at = x_candidates[i+1]
                 continue
             else:
-                print "crop()ing at ", (start_at, 0, crop_at, height)
+                if debug: print "crop()ing at ", (start_at, 0, crop_at, height)
                 c = strip.crop((start_at, 0, crop_at, height))
                 # c.show()
                 frames.append(c)
-                start_at = x_cord
-
-        print "crop()ing at ", (start_at, 0, width, height)
-        c = strip.crop((start_at, 0, width, height))
-        # c.show()
-        frames.append(c)
+                start_at = crop_at
 
     for index, frame in enumerate(frames):
-        frame_name = "%.2d-%s" % (index, filename)
+        frame_name = os.path.join(odir, "%.2d-%s" % (index, ofile))
         print "writing frame %d as %s" % (index, frame_name)
         fl = open(frame_name, 'w')
         frame.save(fl)
